dfbf7f30dcda9aa580ce53f0a143aaa6aba534c1
Leon Wang <seasparta618@gmail.com>
2024-02-01 18:31:49 +1100

    Story: added search bar full story

---
+++ storybook/stories/atoms/form/search-bar/SearchBar.stories.ts
@@ -0,0 +1,28 @@
+import { BuildSearchBar } from './SearchBar';
+
+export default {
+  title: '/Atoms/Form/Search Bar',
+  component: BuildSearchBar,
+  argTypes: {
+    placeholder: {
+      control: { type: 'text' },
+      defaultValue: 'Search by name, email or role',
+      description: 'Placeholder text for the search bar',
+    },
+    defaultValue: {
+      control: { type: 'text' },
+      defaultValue: '',
+      description: 'Default value for the search bar',
+    }
+  },
+};
+
+export const SearchBar = (args) => {
+  return BuildSearchBar ({...args});
+};
+
+SearchBar.args = {
+  placeholder: 'Search by name, email or role',
+  defaultValue: '',
+  onSubmit:(value) => console.log('The submitted search content is:', value)
+};
---
+++ storybook/stories/atoms/form/search-bar/SearchBar.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import './search-bar.scss';
+import '../../button/button.scss';
+import { CrossIcon, SearchIcon } from "../../../shared/icon";
+
+interface SearchBarProps {
+    defaultValue: string;
+    placeholder: string;
+    onSubmit?: (value: string) => void;
+}
+
+export const BuildSearchBar = ({
+    placeholder = 'Search by name, email or role',
+    defaultValue = '',
+    onSubmit
+}: SearchBarProps) => {
+    const [value, setValue] = useState<string>(defaultValue);
+    const [isComposing, setIsComposing] = useState<boolean>(false);
+
+    useEffect(() => {
+        setValue(defaultValue);
+    }, [defaultValue])
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+    };
+
+    const handleCompositionStart = () => {
+        setIsComposing(true);
+    };
+
+    const handleCompositionEnd = () => {
+        setIsComposing(false);
+    };
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter' && !isComposing) {
+            onSubmit && onSubmit(value);
+        }
+    };
+
+    return (
+        <div className="searchBar">
+            <span className="searchBar-icon">
+                <SearchIcon size={20} />
+            </span>
+            <input
+                className="searchBar-input"
+                placeholder={placeholder}
+                value={value}
+                onChange={handleChange}
+                onCompositionStart={handleCompositionStart}
+                onCompositionEnd={handleCompositionEnd}
+                onKeyDown={handleKeyDown}
+            />
+            {value.length ? <button className="button-sm button-primary">Search</button> : null}
+            {value.length ? <span className="searchBar-icon searchBar-cleanupIcon" onClick={()=>{setValue('')}}>
+                <CrossIcon size={20} />
+            </span> : null
+            }
+        </div>
+    );
+};
