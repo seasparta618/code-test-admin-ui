be6521621e93bd77ccb0d041565382138efb424b
Leon Wang <seasparta618@gmail.com>
2024-02-01 22:00:49 +1100

    set up basic page content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { AdminPage } from './components/pages/adminPage';
 import './styles/main.scss';
 
 export const App = () => {
   return (
     <div className="mainPage" data-testid="main-page">
       <div className="mainPage-header" data-testid="page-header"></div>
-      <div className="mainPage-content" data-testid="page-content"></div>
+      <div className="mainPage-content" data-testid="page-content">
+        <AdminPage />
+      </div>
     </div>
   );
 };
