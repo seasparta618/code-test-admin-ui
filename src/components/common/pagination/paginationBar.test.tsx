5f1cb6ce87ece1231e98321296ab2c8eaccb2605
Leon Wang <seasparta618@gmail.com>
2024-02-01 23:06:36 +1100

    add pagination bar and the test

---
+++ src/components/common/pagination/paginationBar.test.tsx
@@ -0,0 +1,113 @@
+// PaginationBar.test.tsx
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { PaginationBar } from './paginationBar';
+
+describe('PaginationBar', () => {
+    const handlePageChange = jest.fn();
+
+    it('disables the next button and last button when on the last page', () => {
+        render(
+            <PaginationBar
+                currentPage={5}
+                totalPage={5}
+                totalButtonNumber={5}
+                onPageChange={handlePageChange}
+            />
+        );
+        const nextButton = screen.getByTestId('pagination-next');
+        expect(nextButton).toHaveClass('button-disabled');
+        const lastButton = screen.getByTestId('pagination-last');
+        expect(lastButton).toHaveClass('button-disabled');
+        const paginationBar = screen.getByTestId('pagination-bar');
+        const allButtons = paginationBar.children;
+        expect(allButtons).toHaveLength(9);
+    });
+
+    it('disables the first button and previous button when on the first page', () => {
+        render(
+            <PaginationBar
+                currentPage={1}
+                totalPage={10}
+                totalButtonNumber={5}
+                onPageChange={handlePageChange}
+            />
+        );
+        const nextButton = screen.getByTestId('pagination-first');
+        expect(nextButton).toHaveClass('button-disabled');
+        const lastButton = screen.getByTestId('pagination-previous');
+        expect(lastButton).toHaveClass('button-disabled');
+        const paginationBar = screen.getByTestId('pagination-bar');
+        const allButtons = paginationBar.children;
+        expect(allButtons).toHaveLength(9);
+    });
+
+    it('calls onPageChange with 0 when click on next button and last button at last page', () => {
+        render(
+            <PaginationBar
+                currentPage={10}
+                totalPage={10}
+                totalButtonNumber={5}
+                onPageChange={handlePageChange}
+            />
+        );
+        const nextButton = screen.getByTestId('pagination-next');
+        fireEvent.click(nextButton);
+        expect(handlePageChange).not.toHaveBeenCalled();
+        const lastButton = screen.getByTestId('pagination-last');
+        fireEvent.click(lastButton);
+        expect(handlePageChange).not.toHaveBeenCalled();
+    });
+
+    it('onPageChange will not been called when click on next button and last button at last page', () => {
+        render(
+            <PaginationBar
+                currentPage={10}
+                totalPage={10}
+                totalButtonNumber={5}
+                onPageChange={handlePageChange}
+            />
+        );
+        const nextButton = screen.getByTestId('pagination-next');
+        fireEvent.click(nextButton);
+        expect(handlePageChange).not.toHaveBeenCalled();
+        const lastButton = screen.getByTestId('pagination-last');
+        fireEvent.click(lastButton);
+        expect(handlePageChange).not.toHaveBeenCalled();
+    });
+
+    it('onPageChange will not been called when click on first button and previous button at first page', () => {
+        render(
+            <PaginationBar
+                currentPage={1}
+                totalPage={10}
+                totalButtonNumber={5}
+                onPageChange={handlePageChange}
+            />
+        );
+        const firstButton = screen.getByTestId('pagination-first');
+        fireEvent.click(firstButton);
+        expect(handlePageChange).not.toHaveBeenCalled()
+        const previousButton = screen.getByTestId('pagination-previous');
+        fireEvent.click(previousButton);
+        expect(handlePageChange).not.toHaveBeenCalledWith();
+    });
+
+    it('calls onPageChange with the first and last button', () => {
+        render(
+            <PaginationBar
+                currentPage={4}
+                totalPage={10}
+                totalButtonNumber={5}
+                onPageChange={handlePageChange}
+            />
+        );
+        const firstButton = screen.getByTestId('pagination-first');
+        fireEvent.click(firstButton);
+        expect(handlePageChange).toHaveBeenCalledWith(1);
+        const lastButton = screen.getByTestId('pagination-last');
+        fireEvent.click(lastButton);
+        expect(handlePageChange).toHaveBeenCalledWith(10);
+    });
+});
+
---
+++ src/utils/usePagination.test.ts
@@ -0,0 +1,48 @@
+import { renderHook } from '@testing-library/react';
+import { usePagination } from './usePagination';
+
+describe('usePagination', () => {
+  it('should return all pages when total pages are less than max buttons', () => {
+    const { result } = renderHook(() =>
+      usePagination({ currentPage: 1, totalPage: 3, totalButtonNumber: 5 })
+    );
+    expect(result.current).toEqual([1, 2, 3]);
+  });
+
+  it('should return pages starting from 1 when current page is near the start', () => {
+    const { result } = renderHook(() =>
+      usePagination({ currentPage: 2, totalPage: 10, totalButtonNumber: 5 })
+    );
+    expect(result.current).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('should return pages starting from 1 when current page is near the start', () => {
+    const { result } = renderHook(() =>
+      usePagination({ currentPage: 4, totalPage: 10, totalButtonNumber: 6 })
+    );
+    expect(result.current).toEqual([2, 3, 4, 5, 6, 7]);
+  });
+
+  it('should return last pages when current page is near the end', () => {
+    const { result } = renderHook(() =>
+      usePagination({ currentPage: 76, totalPage: 109, totalButtonNumber: 11 })
+    );
+    expect(result.current).toEqual([
+      71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81,
+    ]);
+  });
+
+  it('should try to center the current page', () => {
+    const { result } = renderHook(() =>
+      usePagination({ currentPage: 5, totalPage: 1000, totalButtonNumber: 5 })
+    );
+    expect(result.current).toEqual([3, 4, 5, 6, 7]);
+  });
+
+  it('should return undefined when total page is 0', () => {
+    const { result } = renderHook(() =>
+      usePagination({ currentPage: 1, totalPage: 0, totalButtonNumber: 5 })
+    );
+    expect(result.current).toEqual([]);
+  });
+});
