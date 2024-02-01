778da7fa02a542f0a7361ed37239b27fefd65d5d
Leon Wang <seasparta618@gmail.com>
2024-02-01 21:10:55 +1100

    add color util and basic icon components

---
+++ src/components/icons/icon.tsx
@@ -0,0 +1,56 @@
+import type { FC, SVGProps } from "react";
+import { ColorEnum } from "../../utils/color";
+
+export interface IconProps extends SVGProps<SVGSVGElement> {
+  size?: number;
+  color?: string;
+  strokeWidth?: string;
+}
+
+const defaultIconProps: IconProps = {
+  size: 24,
+  color: ColorEnum.GREY,
+  strokeWidth: "2",
+};
+
+export const EditIcon: FC<IconProps> = ({ size, color, ...props }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12 20H21M3.00003 20H4.67457C5.16376 20 5.40835 20 5.63852 19.9447C5.84259 19.8957 6.03768 19.8149 6.21663 19.7053C6.41846 19.5816 6.59141 19.4086 6.93732 19.0627L19.5001 6.49998C20.3285 5.67156 20.3285 4.32841 19.5001 3.49998C18.6716 2.67156 17.3285 2.67156 16.5001 3.49998L3.93729 16.0627C3.59139 16.4086 3.41843 16.5816 3.29475 16.7834C3.18509 16.9624 3.10428 17.1574 3.05529 17.3615C3.00003 17.5917 3.00003 17.8363 3.00003 18.3255V20Z"
+        stroke={color}
+        strokeWidth={props.strokeWidth ?? 2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
+  );
+};
+EditIcon.defaultProps = defaultIconProps;
+
+export const RemoveIcon: FC<IconProps> = ({ size, color, ...props }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16 6V5.2C16 4.0799 16 3.51984 15.782 3.09202C15.5903 2.71569 15.2843 2.40973 14.908 2.21799C14.4802 2 13.9201 2 12.8 2H11.2C10.0799 2 9.51984 2 9.09202 2.21799C8.71569 2.40973 8.40973 2.71569 8.21799 3.09202C8 3.51984 8 4.0799 8 5.2V6M10 11.5V16.5M14 11.5V16.5M3 6H21M19 6V17.2C19 18.8802 19 19.7202 18.673 20.362C18.3854 20.9265 17.9265 21.3854 17.362 21.673C16.7202 22 15.8802 22 14.2 22H9.8C8.11984 22 7.27976 22 6.63803 21.673C6.07354 21.3854 5.6146 20.9265 5.32698 20.362C5 19.7202 5 18.8802 5 17.2V6"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
+  );
+};
+RemoveIcon.defaultProps = defaultIconProps;
---
+++ src/utils/color.ts
@@ -0,0 +1,7 @@
+export enum ColorEnum {
+    BLACK = "#000",
+    WHITE = "#fff",
+    PURPLE = "#FF0000",
+    GREY = "#808080",
+  }
+  
