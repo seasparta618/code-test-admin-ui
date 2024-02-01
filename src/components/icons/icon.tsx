599d52ff4863ab21702ee621becdb3ad890d26ed
Leon Wang <seasparta618@gmail.com>
2024-02-01 21:12:07 +1100

    add more basic icons

--- src/components/icons/icon.tsx
+++ src/components/icons/icon.tsx
@@ -1,5 +1,5 @@
-import type { FC, SVGProps } from "react";
-import { ColorEnum } from "../../utils/color";
+import type { FC, SVGProps } from 'react';
+import { ColorEnum } from '../../utils/color';
 
 export interface IconProps extends SVGProps<SVGSVGElement> {
   size?: number;
@@ -10,7 +10,7 @@ export interface IconProps extends SVGProps<SVGSVGElement> {
 const defaultIconProps: IconProps = {
   size: 24,
   color: ColorEnum.GREY,
-  strokeWidth: "2",
+  strokeWidth: '2',
 };
 
 export const EditIcon: FC<IconProps> = ({ size, color, ...props }) => {
@@ -54,3 +54,137 @@ export const RemoveIcon: FC<IconProps> = ({ size, color, ...props }) => {
   );
 };
 RemoveIcon.defaultProps = defaultIconProps;
+
+export const SearchIcon: FC<IconProps> = ({ size, color, ...props }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M21 21L15.0001 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+SearchIcon.defaultProps = defaultIconProps;
+
+export const CrossIcon: FC<IconProps> = ({ size, color, ...props }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18 6L6 18M6 6L18 18"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+CrossIcon.defaultProps = defaultIconProps;
+
+export const DoubleChevronLeftIcon: FC<IconProps> = ({
+  size,
+  color,
+  ...props
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M18 17L13 12L18 7M11 17L6 12L11 7"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+DoubleChevronLeftIcon.defaultProps = defaultIconProps;
+
+export const DoublChevronRightIcon: FC<IconProps> = ({
+  size,
+  color,
+  ...props
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M6 17L11 12L6 7M13 17L18 12L13 7"
+        stroke={color}
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+DoublChevronRightIcon.defaultProps = defaultIconProps;
+
+export const ChevronLeftIcon: FC<IconProps> = ({ size, color, ...props }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M15 18L9 12L15 6"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+ChevronLeftIcon.defaultProps = defaultIconProps;
+
+export const ChevronRightIcon: FC<IconProps> = ({ size, color, ...props }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M9 18L15 12L9 6"
+        stroke={color}
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+ChevronRightIcon.defaultProps = defaultIconProps;
--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,7 +1,6 @@
 export enum ColorEnum {
-    BLACK = "#000",
-    WHITE = "#fff",
-    PURPLE = "#FF0000",
-    GREY = "#808080",
-  }
-  
+  BLACK = '#000',
+  WHITE = '#fff',
+  PURPLE = '#FF0000',
+  GREY = '#808080',
+}
