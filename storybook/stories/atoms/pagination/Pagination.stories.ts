f55e60f2f527649ed90b3d55420cd6a083d5684e
Leon Wang <seasparta618@gmail.com>
2024-02-01 18:54:01 +1100

    Story: add pagination story

---
+++ storybook/stories/atoms/pagination/Pagination.stories.ts
@@ -0,0 +1,32 @@
+import { BuildPagination } from './Pagination';
+
+export default {
+  title: '/Atoms/Pagination',
+  argTypes: {
+    initialSelectedPage: {
+      control: { type: 'select' },
+      options: [1,2,3,4,5],
+      description: 'The initial selected page',
+    },
+    pageRange: {
+      control: { type: 'object' },
+      defaultValue: [1, 2, 3, 4, 5],
+      description: 'The range of pages to display',
+    },
+    buttonStyle: {
+      control: {type:'select'},
+      options: ['primary', 'secondary', 'tertiary', 'quatenary'],
+      description: 'select the button type',
+    }
+  },
+};
+
+export const Pagination = (args) => {
+  return BuildPagination({...args});
+};
+Pagination.args = {
+    initialSelectedPage: 1,
+    pageRange: [1, 2, 3, 4, 5],
+    buttonStyle: 'secondary',
+  };
+  
